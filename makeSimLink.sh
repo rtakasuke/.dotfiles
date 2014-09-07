016ab987ab0a1ebd3ffcac6770821804bf6c1b48
rtakasuke <takahashi@tamagohan.skr.jp>
2014-09-07 10:24:33 +0900

    ざっくり

--- makeSimLink.sh
+++ makeSimLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.git ~/.git
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
