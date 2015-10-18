c5b4e66618d765b407d0028c2eb8a5a52d170b53
rtakasuke <takahashi@tamagohan.skr.jp>
2015-10-18 14:23:59 +0900

    update

---
+++ setupCommands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BIN_DIR=~/bin
+MKDIR_CMD=/bin/mkdir
+CURL_CMD=/usr/bin/curl
+
+${MKDIR_CMD} -p ${BIN_DIR}
+
+# ack
+${CURL_CMD} -sL http://betterthangrep.com/ack-standalone > ${BIN_DIR}/ack
+chmod +x ${BIN_DIR}/ack
+
+# git-ignore
+${CURL_CMD} -sL https://raw.githubusercontent.com/yuroyoro/git-ignore/master/git-ignore > ${BIN_DIR}/git-ignore
+chmod +x ${BIN_DIR}/git-ignore
