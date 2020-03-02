d7a1f84c15499b70a01e99093e1a1c3759969496
rtakasuke <takahashi@tamagohan.skr.jp>
2020-03-03 02:53:45 +0900

    update

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,12 +9,6 @@ ln -sf $BASE_DIR/gitconfig ~/.gitconfig
 ln -sf $BASE_DIR/tigrc ~/.tigrc
 ln -sf $BASE_DIR/nvim ~/.config/nvim
 
-echo "### Install dein.vim (vim)"
-DEIN_VIM_INSTALLER=$BASE_DIR/dein_vim_installer.sh
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $DEIN_VIM_INSTALLER
-sh $DEIN_VIM_INSTALLER ~/.vim/dein
-rm $DEIN_VIM_INSTALLER
-
 echo "### Install dein.vim (neovim)"
 DEIN_VIM_INSTALLER=$BASE_DIR/dein_vim_installer.sh
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $DEIN_VIM_INSTALLER
