7bef96dbec4c91a2a7c56c7c4c5b9f1b5ff62268
rtakasuke <takahashi@tamagohan.skr.jp>
2020-03-12 01:21:53 +0900

    updage

--- bin/init.sh
+++ bin/init.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-BASE_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
-CONFIG_DIR=~/.config
-
 # CI 例外処理用
 set -e
 
-if [ ! -d ${CONFIG_DIR} ]
+BASE_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
+
+echo -e "\n### Create ~/.config directory"
+CONFIG_DIR=~/.config
+if [ ! -d "$CONFIG_DIR" ]
 then
-    mkdir ${CONFIG_DIR}
+    mkdir "$CONFIG_DIR"
+    echo "created."
+else
+    echo "already exit."
 fi
 
-echo "### Create symlinks for dotfiles"
-ln -sf "$BASE_DIR"/bash_profile ~/.bash_profile
-ln -sf "$BASE_DIR"/bashrc       ~/.bashrc
-ln -sf "$BASE_DIR"/gitconfig    ~/.gitconfig
-ln -sf "$BASE_DIR"/tigrc        ~/.tigrc
-ln -sf "$BASE_DIR"/zshenv       ~/.zshenv
-ln -sf "$BASE_DIR"/zshrc        ~/.zshrc
-ln -sf "$BASE_DIR"/nvim         "$CONFIG_DIR"/nvim
+echo -e "\n### Create symlinks for dotfiles"
+ln -vsf "$BASE_DIR"/bash_profile ~/.bash_profile
+ln -vsf "$BASE_DIR"/bashrc       ~/.bashrc
+ln -vsf "$BASE_DIR"/gitconfig    ~/.gitconfig
+ln -vsf "$BASE_DIR"/tigrc        ~/.tigrc
+ln -vsf "$BASE_DIR"/zshenv       ~/.zshenv
+ln -vsf "$BASE_DIR"/zshrc        ~/.zshrc
+ln -vsf "$BASE_DIR"/nvim         "$CONFIG_DIR"/nvim
 
-echo "### Install dein.vim (neovim)"
+echo -e "\n### Install dein.vim (for neovim)"
 DEIN_CACHE_DIR=~/.cache/dein
+DEIN_VIM_INSTALLER=$BASE_DIR/dein_vim_installer.sh
 if [ -f ${DEIN_CACHE_DIR}/repos/github.com/Shougo/dein.vim ]; then
-    DEIN_VIM_INSTALLER=$BASE_DIR/dein_vim_installer.sh
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "$DEIN_VIM_INSTALLER"
     sh "$DEIN_VIM_INSTALLER" "$DEIN_CACHE_DIR"
     rm "$DEIN_VIM_INSTALLER"
 else
-    echo "dein.vim already installed."
+    echo "already installed."
 fi
 
 echo -e "\n[Finish]"
