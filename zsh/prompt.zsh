88b3add15c5dbf94da938fcfbbf63d77c51d2bd5
rtakasuke <rtakasuke@iMac.local>
2020-09-26 16:45:02 +0900

    update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -22,11 +22,8 @@ zstyle ':vcs_info:git:*' formats       "%F{green}%b%u%c"
 zstyle ':vcs_info:git:*' actionformats "%F{green}%b%u%c%F{green}|%f%F{red}%a%f"
 local p_git='${vcs_info_msg_0_}'
 
-# pyenv
-local p_pyenv='%F{green}py:$(pyenv version-name)%f'
-
 # General
 local p_br=$'\n'
 local p_mark="%B%(?,%F{green},%F{red})>%f%b"
-local p_info="%F{green}(%f$p_git$p_pyenv%F{green})%f"
+local p_info="%F{green}(%f$p_git%F{green})%f"
 PROMPT="$p_dir $p_info $p_br$p_mark "
